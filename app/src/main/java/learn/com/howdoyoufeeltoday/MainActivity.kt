338d0d993f11a1f0d071ad4dd33c1ac6c1255e47
Administrator <trangtrunghoangphuc@gmail.com>
2017-06-05 10:04:16 +0700

    New features: Face emotion detection

--- app/src/main/java/learn/com/howdoyoufeeltoday/MainActivity.kt
+++ app/src/main/java/learn/com/howdoyoufeeltoday/MainActivity.kt
@@ -8,7 +8,11 @@ import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.net.Uri
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.RecognizerIntent
@@ -19,13 +23,21 @@ import android.util.Log
 import android.view.KeyEvent
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
-import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import com.google.gson.Gson
+import com.microsoft.projectoxford.emotion.EmotionServiceClient
+import com.microsoft.projectoxford.emotion.EmotionServiceRestClient
+import com.microsoft.projectoxford.emotion.contract.FaceRectangle
+import com.microsoft.projectoxford.emotion.contract.Order
+import com.microsoft.projectoxford.emotion.contract.RecognizeResult
+import com.microsoft.projectoxford.face.FaceServiceRestClient
 import kotlinx.android.synthetic.main.activity_main.*
 import learn.com.howdoyoufeeltoday.helper.ImageHelper
 import learn.com.howdoyoufeeltoday.helper.SelectImageActivity
 import learn.com.howdoyoufeeltoday.helper.ViewImageActivity
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
 import java.util.*
 
 
@@ -41,6 +53,7 @@ class MainActivity : AppCompatActivity() {
 
     // The image selected to detect.
     var mBitmap: Bitmap? = null
+    var client: EmotionServiceClient? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -114,6 +127,10 @@ class MainActivity : AppCompatActivity() {
         rvConversation.layoutManager=layoutManager
         conversationList.add(Conversation("How do you feel today?",0))
         conversationAdapter.notifyDataSetChanged()
+        //Add Client for recognize emotion
+        if (client == null) {
+            client = EmotionServiceRestClient(getString(R.string.subscription_key))
+        }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -155,26 +172,162 @@ class MainActivity : AppCompatActivity() {
                     if (mBitmap != null) {
                         // Show the image on screen.
                         val newIntent=Intent(this,ViewImageActivity::class.java)
-                        newIntent.putExtra("URI",mImageUri)
+                        newIntent.putExtra("URI",mImageUri.toString())
                         startActivityForResult(newIntent,viewImageRequestCode)
                     }
                 }
             }
             viewImageRequestCode->{
-                val okOrNot=data?.getIntExtra("OK_OR_NOT",1)
-                if (okOrNot==0){
-                    val intent=Intent(this,SelectImageActivity::class.java)
-                    startActivityForResult(intent,selectImageRequestCode)
-                }else {
-                    doRecognize(mImageUri)
+                if (resultCode== Activity.RESULT_OK) {
+                    val okOrNot = data?.getIntExtra("OK_OR_NOT", 1)
+                    if (okOrNot == 0) {
+                        val intent = Intent(this, SelectImageActivity::class.java)
+                        startActivityForResult(intent, selectImageRequestCode)
+                    } else {
+                        doRecognize()
+                    }
                 }
             }
         }
         super.onActivityResult(requestCode, resultCode, data)
     }
 
-    private fun doRecognize(mImageUri: Uri?) {
+    private fun doRecognize() {
+        try {
+            doRequest(false).execute()
+        } catch (e: Exception) {
+            processResponse("Error encountered. Exception is: " + e.toString())
+        }
 
+
+        /*val faceSubscriptionKey = getString(R.string.faceSubscription_key)
+        if (faceSubscriptionKey.equals("Please_add_the_face_subscription_key_here", ignoreCase = true)) {
+            processResponse("\n\nThere is no face subscription key in res/values/strings.xml. Skip the sample for detecting emotions using face rectangles\n")
+        } else {
+            // Do emotion detection using face rectangles provided by Face API.
+            try {
+                doRequest(true).execute()
+            } catch (e: Exception) {
+                processResponse("Error encountered. Exception is: " + e.toString())
+            }
+
+        }*/
     }
 
+    //AsyncTask helps push request to Microsoft server
+    inner class doRequest(useFaceRectangle: Boolean) : AsyncTask<String, String, List<RecognizeResult>>() {
+        var useFaceRectangles=useFaceRectangle
+        override fun doInBackground(vararg params: String?): List<RecognizeResult>? {
+            /*if (!useFaceRectangles) {*/
+                try {
+                    return processWithAutoFaceDetection()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    return null
+                }
+
+            /*} else {
+                try {
+                    return processWithFaceRectangles()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }*/
+
+        }
+
+        override fun onPostExecute(result: List<RecognizeResult>?) {
+            super.onPostExecute(result)
+            if(result?.size==0){
+                processResponse("No emotion detected")
+            }else{
+                val bitmapCopy = mBitmap?.copy(Bitmap.Config.ARGB_8888, true)
+                val faceCanvas = Canvas(bitmapCopy)
+                faceCanvas.drawBitmap(mBitmap, 0f, 0f, null)
+                val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+                paint.style = Paint.Style.STROKE
+                paint.strokeWidth = 5f
+                paint.color = Color.RED
+                //Only choose the first face
+                val r=result?.get(0)
+                val collection : List<Map.Entry<String, Double>>? = r?.scores?.ToRankedList(Order.DESCENDING)
+                val emotion=collection?.get(0)?.key.toString()
+
+                processResponse(emotion)
+            }
+        }
+    }
+
+    private fun processWithFaceRectangles(): List<RecognizeResult>? {
+        val gson = Gson()
+
+        // Put the image into an input stream for detection.
+        val output = ByteArrayOutputStream()
+        mBitmap?.compress(Bitmap.CompressFormat.JPEG, 100, output)
+        val inputStream = ByteArrayInputStream(output.toByteArray())
+
+        var timeMark = System.currentTimeMillis()
+        Log.d("emotion", "Start face detection using Face API")
+        var faceRectangles:Array<FaceRectangle?> = arrayOfNulls<FaceRectangle>(0)
+        val faceSubscriptionKey = getString(R.string.faceSubscription_key)
+        val faceClient = FaceServiceRestClient(faceSubscriptionKey)
+        val faces = faceClient.detect(inputStream, false, false, null)
+        Log.d("emotion", String.format("Face detection is done. Elapsed time: %d ms", System.currentTimeMillis() - timeMark))
+
+        if (faces != null) {
+            faceRectangles= arrayOfNulls<FaceRectangle>(faces.size)
+
+            for (i in faceRectangles.indices) {
+                // Face API and Emotion API have different FaceRectangle definition. Do the conversion.
+                val rect = faces[i].faceRectangle
+                faceRectangles[i] = com.microsoft.projectoxford.emotion.contract.FaceRectangle(rect.left, rect.top, rect.width, rect.height)
+            }
+        }
+
+        var result: List<RecognizeResult>? = null
+        if (faceRectangles != null) {
+            inputStream.reset()
+
+            timeMark = System.currentTimeMillis()
+            Log.d("emotion", "Start emotion detection using Emotion API")
+            // -----------------------------------------------------------------------
+            // KEY SAMPLE CODE STARTS HERE
+            // -----------------------------------------------------------------------
+            result = this.client?.recognizeImage(inputStream, faceRectangles)
+
+            val json = gson.toJson(result)
+            Log.d("result", json)
+            // -----------------------------------------------------------------------
+            // KEY SAMPLE CODE ENDS HERE
+            // -----------------------------------------------------------------------
+            Log.d("emotion", String.format("Emotion detection is done. Elapsed time: %d ms", System.currentTimeMillis() - timeMark))
+        }
+        return result
+    }
+
+    private fun processWithAutoFaceDetection(): List<RecognizeResult>?{
+        Log.d("emotion", "Start emotion detection with auto-face detection")
+        val gson = Gson()
+
+        // Put the image into an input stream for detection.
+        val output = ByteArrayOutputStream()
+        mBitmap?.compress(Bitmap.CompressFormat.JPEG, 100, output)
+        val inputStream = ByteArrayInputStream(output.toByteArray())
+
+        val startTime = System.currentTimeMillis()
+        // -----------------------------------------------------------------------
+        // KEY SAMPLE CODE STARTS HERE
+        // -----------------------------------------------------------------------
+
+        var result: List<RecognizeResult>? = null
+        //
+        // Detect emotion by auto-detecting faces in the image.
+        //
+        result = this.client?.recognizeImage(inputStream)
+
+        val json = gson.toJson(result)
+        Log.d("result", json)
+        Log.d("emotion", String.format("Detection done. Elapsed time: %d ms", System.currentTimeMillis() - startTime))
+        return result
+    }
 }
--- app/src/main/java/learn/com/howdoyoufeeltoday/helper/ViewImageActivity.kt
+++ app/src/main/java/learn/com/howdoyoufeeltoday/helper/ViewImageActivity.kt
@@ -1,7 +1,11 @@
 package learn.com.howdoyoufeeltoday.helper
 
+import android.app.Activity
+import android.graphics.Bitmap
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_view_image.*
 
 import learn.com.howdoyoufeeltoday.R
 
@@ -10,5 +14,23 @@ class ViewImageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_image)
+        val intent=intent
+        val uriString=intent.getStringExtra("URI")
+        val photoUri= Uri.parse(uriString)
+        val bitmap:Bitmap=ImageHelper.loadSizeLimitedBitmapFromUri(
+                photoUri, contentResolver)
+        if (bitmap!=null){
+            imgPhoto.setImageBitmap(bitmap)
+        }
+        btnOk.setOnClickListener {
+            intent.putExtra("OK_OR_NOT",1)
+            setResult(Activity.RESULT_OK,intent)
+            finish()
+        }
+        btnCancel.setOnClickListener {
+            intent.putExtra("OK_OR_NOT",0)
+            setResult(Activity.RESULT_OK,intent)
+            finish()
+        }
     }
 }
